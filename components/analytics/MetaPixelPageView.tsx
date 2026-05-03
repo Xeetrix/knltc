6d1ab4d287afdd0a0586283db98b590abe386016
Shaikh Baset <148635255+shaikhbaset2001@users.noreply.github.com>
2026-05-03 20:05:40 +0600

    Add global Meta Pixel integration for App Router

--- components/analytics/MetaPixelPageView.tsx
+++ components/analytics/MetaPixelPageView.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-import { useEffect, useMemo } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
-import { trackPageView } from "@/lib/meta-pixel";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { fbq } from "@/lib/fbq";
 
 export default function MetaPixelPageView() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  const queryString = searchParams.toString();
-
-  const pathWithQuery = useMemo(() => (queryString ? `${pathname}?${queryString}` : pathname), [pathname, queryString]);
 
   useEffect(() => {
-    trackPageView(pathWithQuery);
-  }, [pathWithQuery]);
+    if (!pathname) return;
+    fbq("track", "PageView");
+  }, [pathname]);
 
   return null;
 }
